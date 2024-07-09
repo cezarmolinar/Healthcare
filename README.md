caf5dfb9402472e203851ace1b474d3bcdd78c04
Cezar Molinar <cezarmolinar@gmail.com>
2024-07-09 08:59:01 -0300

    First step of onboarding

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Healthcare
-The project was developed to train knowledge in JavaScript technology (Next.js more specificity). The code was developed based on  https://www.youtube.com/watch?v=lEflo_sc82g&t=1586s
+
+The project was developed to train knowledge in JavaScript technology (Next.js more specificity). The code was developed based on https://www.youtube.com/watch?v=lEflo_sc82g&t=1586s
 
 A healthcare patient management application that allows patients to easily register, book, and manage their appointments with doctors, featuring administrative tools for scheduling, confirming, and canceling appointments, along with SMS notifications.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,33 @@
-import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from 'next'
+import { Plus_Jakarta_Sans } from 'next/font/google'
+import './globals.css'
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/theme-provider'
 
-const fontSans = Plus_Jakarta_Sans({ 
-  subsets: ["latin"],
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-sans'
-});
+})
 
 export const metadata: Metadata = {
-  title: "CarePulse",
-  description: "A healthcare management system",
-};
+  title: 'CarePulse',
+  description: 'A healthcare management system'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+      <body
+        className={cn(
+          'min-h-screen bg-dark-300 font-sans antialiased',
+          fontSans.variable
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
@@ -33,5 +38,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
